03f3224d51ceb4b388f87669edbcba486e014cb6
knud21kea <grah0070@stud.kea.dk>
2023-11-12 17:38:23 +0100

    sign up and log in working

--- nm2/server/databases/createDatabase.js
+++ nm2/server/databases/createDatabase.js
@@ -1,31 +1,30 @@
-import db from "./connection.js"
+import db from "./connection.js";
 import bcrypt from "bcrypt";
 
-const isDeleteMode = process.argv.findIndex((argument) => argument === "delete_mode") === -1 ? false : true;
+const isDeleteMode = process.argv.findIndex((arg) => arg === "delete") === -1 ? false : true;
 
-if (isDeleteMode){
-    db.exec('DROP TABLE users;');
+
+if (isDeleteMode) {
+    await db.exec("DROP TABLE IF EXISTS users;");
 }
 
-/* DDL */
-
-db.exec(`
-CREATE TABLE IF NOT EXISTS users(
+// Create the tables (DDL)
+await db.exec(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT,
     password TEXT
-    );
-`);
+);`);
 
-/* DML - seeding */
+
+// SEED the database (DML)
 
 const anniPw = await bcrypt.hash("anniCode", 12);
 const bibiPw = await bcrypt.hash("bibiCode", 12);
 const carlPw = await bcrypt.hash("carlCode", 12);
 
-if(isDeleteMode){
-db.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ['Anni', 'a@mail.dk', anniPw]);
-db.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ['Bibi', 'b@mail.dk', bibiPw]);
-db.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ['Carl', 'c@mail.dk', carlPw]);
+if (isDeleteMode) {
+    db.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ['Anni', 'a@mail.dk', anniPw]);
+    db.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ['Bibi', 'b@mail.dk', bibiPw]);
+    db.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", ['Carl', 'c@mail.dk', carlPw]);
 }
