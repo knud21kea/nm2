34890a4754e41189df003231b2c5152fdf198c53
knud21kea <grah0070@stud.kea.dk>
2023-11-12 15:55:43 +0100

    client components

---
+++ nm2/client/src/components/Protectors/PrivateRoute.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Route } from "svelte-navigator";
+    import PrivateRouteGuard from "./PrivateRouteGuard.svelte";
+    let navigate;
+    let location;
+    export let path;
+</script>
+
+<!-- The path prop {path} is used to render the protected component if the user is authenticated -->
+
+<Route {path}>
+    <PrivateRouteGuard {navigate} {location}>
+        <slot/>
+    </PrivateRouteGuard>
+</Route>
---
+++ nm2/client/src/components/Protectors/PrivateRouteGuard.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { useNavigate, useLocation } from "svelte-navigator";
+    import { user } from "../../store/user.js";
+    export let navigate = useNavigate();
+    export let location = useLocation();
+   
+    $: if (!$user) {
+        navigate("/", {
+            state: { from: $location.pathname },
+            replace: true
+        });
+    }
+</script>
+
+ <slot/>
